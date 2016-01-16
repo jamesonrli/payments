57615f430be3ae53b64e55a7bd4c68a70ce4c19c
Jameson Li <jamesonrli@gmail.com>
2016-01-16 00:32:13 -0800

    init with signup frag

---
+++ app/src/main/java/com/jamesonli/paymentdemo/checkout/CheckoutActivity.java
@@ -0,0 +1,14 @@
+package com.jamesonli.paymentdemo.checkout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.jamesonli.paymentdemo.R;
+
+public class CheckoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
