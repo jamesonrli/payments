642cfa501c0bb7b9bd68931385d2ecff0e0d11b8
Jameson Li <jamesonrli@gmail.com>
2016-01-16 01:02:15 -0800

    update lock image

--- app/src/main/java/com/jamesonli/paymentdemo/checkout/CheckoutActivity.java
+++ app/src/main/java/com/jamesonli/paymentdemo/checkout/CheckoutActivity.java
@@ -1,6 +1,8 @@
 package com.jamesonli.paymentdemo.checkout;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import com.jamesonli.paymentdemo.R;
 
@@ -10,5 +12,12 @@ public class CheckoutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        switchContainerFragment(SignUpFragment.getInstance(), R.id.checkout_container);
+    }
+
+    private void switchContainerFragment(Fragment fragment, int container) {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(container, fragment).commit();
     }
 }
--- app/src/main/java/com/jamesonli/paymentdemo/checkout/SignUpFragment.java
+++ app/src/main/java/com/jamesonli/paymentdemo/checkout/SignUpFragment.java
@@ -12,6 +12,12 @@ import com.jamesonli.paymentdemo.R;
  */
 public class SignUpFragment extends Fragment {
 
+    public static SignUpFragment getInstance() {
+        return new SignUpFragment();
+    }
+
+    public SignUpFragment() {}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_signup, container, false);
